dcc739285ce976ffd9ecb5f1264c4e02d9e08fef
shailendra0102 <sprajjobid@gmail.com>
2024-02-01 10:01:29 +0530

    Could not find declaration file for module 'X' Error

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import { getAllproducts } from './products.ts';
 
-const products = getAllproducts();
+const products = await getAllproducts();
 window.console.log('Hey Webpack with dev server', products);
